84bba9a8266e65ad19fc8e214c07d484aadff931
SurajGardi <surajgardi0707@gmail.com>
2025-05-17 09:36:14 +0530

    addition of 2 number added

---
+++ program53.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class program53 
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enetr 1st Number : ");
+        int no1 = sobj.nextInt();
+
+        System.out.println("Enetr 2nd Number : ");
+        int no2 = sobj.nextInt();
+
+        int Add = no1 + no2;
+        System.out.println("Addition is : "+Add);
+
+    }
+}
