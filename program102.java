1fcdabbe963a821b48e2c05c1b4d48458ee485c3
SurajGardi <surajgardi0707@gmail.com>
2025-05-19 22:40:02 +0530

    pattern program using JAVA added

---
+++ program102.java
@@ -0,0 +1,46 @@
+// Input 4 4
+
+/*
+    1       2       3       4
+    5       6       7       8
+    9       10      11      12
+    13      14      15      16
+ */
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0, iCnt = 0;
+        
+        for(i = 1, iCnt = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++,iCnt++)  
+            {
+                System.out.print(iCnt+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program102
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        
+        pobj.Display(iValue1, iValue2);
+    }
+}
---
+++ program105.java
@@ -0,0 +1,54 @@
+// Input 5 4
+
+/*
+    *       *       *       *
+    *       0       0       *
+    *       0       0       *
+    *       0       0       *
+    *       *       *       *
+ */
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)  
+            {
+                if((j == 1) || (j == icol) || (i == 1)  || (i == iRow))
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("0\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program105
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        
+        pobj.Display(iValue1, iValue2);
+    }
+}
