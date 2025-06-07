cbf12268dc5f0b0bb0ee207aefc6d5a932d81c68
SurajGardi <surajgardi0707@gmail.com>
2025-06-08 00:47:36 +0530

    problem on String CheckPalindrome added

---
+++ program201.c
@@ -0,0 +1,57 @@
+#include<stdio.h>           // Palindrome String with Flag
+#include<stdbool.h>
+
+
+bool CheckPalindrome(char *str)
+{
+    char *start = NULL;
+    char *end = NULL;
+    bool bFlag = true;
+
+    start = str;
+    end = str;
+
+    while(*end != '\0')
+    {
+        end++;
+    }
+    end--;
+
+    while(start < end)
+    {
+        if(*start != *end)
+        {
+            bFlag = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+    return bFlag;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    bool bRet = false;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s", Arr);
+
+    bRet = CheckPalindrome(Arr);
+
+    if(bRet == true)
+    {
+        printf("String is Palindrome");
+    }
+    else
+    {
+        printf("String is not Palindrome");
+    }
+
+    return 0;
+}
+    
+
+// Time Complexity O(N+N/2)
+
---
+++ program202.c
@@ -0,0 +1,62 @@
+#include<stdio.h>               // Palindrome String without Flag
+#include<stdbool.h>
+
+
+bool CheckPalindrome(char *str)
+{
+    char *start = NULL;
+    char *end = NULL;
+
+    start = str;
+    end = str;
+
+    while(*end != '\0')
+    {
+        end++;
+    }
+    end--;
+
+    while(start < end)
+    {
+        if(*start != *end)
+        {
+            break;
+        }
+        start++;
+        end--;
+    }
+    if(start < end)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    bool bRet = false;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s", Arr);
+
+    bRet = CheckPalindrome(Arr);
+
+    if(bRet == true)
+    {
+        printf("String is Palindrome");
+    }
+    else
+    {
+        printf("String is not Palindrome");
+    }
+
+    return 0;
+}
+    
+
+// Time Complexity O(N+N/2)
+
