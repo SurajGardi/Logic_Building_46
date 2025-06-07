aa3ce6e1afa5f293c6635ebeb5df82e1b9f72bd0
SurajGardi <surajgardi0707@gmail.com>
2025-06-08 00:01:59 +0530

    problem on Strings ChkCharacter without Flag by checking '\0' added

---
+++ program193.c
@@ -0,0 +1,49 @@
+#include<stdio.h>       
+#include<stdbool.h>             // Without Flag by checking '\0' 
+
+bool CheckCharacter(char *str, char ch)
+{
+    while(*str != '\0')
+    {
+        if(*str == ch)
+        {
+           break;
+        }
+        str++;
+    }
+    if(*str == '\0')
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    char cValue = '\0';
+    bool bRet = false;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);
+    
+    // Problem due to Buffer cleanup Solved
+    printf("Enter the Character : \n");
+    scanf(" %c",&cValue);       // space before %c 
+
+    bRet = CheckCharacter(Arr,cValue);
+
+    if(bRet == true)
+    {
+        printf("%c is present in %s\n",cValue,Arr);
+    }
+    else
+    {
+        printf("%c is NOT present in %s\n",cValue,Arr);
+    }
+
+    return 0;
+}
