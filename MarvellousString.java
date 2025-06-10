e202d27d59bd4ad51a5b16c77ba8125c3f3a894e
SurajGardi <surajgardi0707@gmail.com>
2025-06-11 00:05:03 +0530

    problem on String CountSmall-III user defined Package for prog213 using JAVA added

---
+++ MarvellousString.java
@@ -0,0 +1,27 @@
+package MarvellousLB;           // client prog : program213.java
+
+public class MarvellousString
+{
+    public String str;
+
+    public MarvellousString(String a)
+    {
+        this.str = a;
+    }
+
+    public int CountSmall()
+    {
+        char Arr[] = str.toCharArray();
+
+        int iCnt = 0, iCount = 0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+}
