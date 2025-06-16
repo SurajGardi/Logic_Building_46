3d92b7aaba3f61276ad220daf7d0a394274b8998
SurajGardi <surajgardi0707@gmail.com>
2025-06-17 00:01:56 +0530

    problem on File Handling read() write() memset added

---
+++ program242.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>       // Required for LINUX OS
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[10] = {'\0'};
+
+    printf("Enter the file name that you want to open : ");
+    scanf("%s",Fname);
+
+    fd = open(Fname, O_RDONLY);        // change
+    if(fd == -1)
+    {
+        printf("Unable to open the file");
+    }
+    else
+    {
+        printf("file succesfully opened with fd : %d\n",fd);
+
+       while((iRet = read(fd,Buffer,10)) != 0)
+       {
+            //printf("%s",Buffer);
+            write(1,Buffer,iRet);
+            memset(Buffer,'\0',10);     // for removing Garbage
+       }
+
+        close(fd);
+    }
+    
+    return 0;
+}
