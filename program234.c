2921378d77fe2a83f5a58eeca4970f58e163fead
SurajGardi <surajgardi0707@gmail.com>
2025-06-16 23:53:28 +0530

    problem on File Handling open() write() read() added

---
+++ program234.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>       // Required for LINUX OS
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[] = "India is My Country";
+
+    printf("Enter the file name that you want to open : ");
+    scanf("%s",Fname);
+
+    fd = open(Fname, O_WRONLY | O_APPEND);        // change
+    if(fd == -1)
+    {
+        printf("Unable to open the file");
+    }
+    else
+    {
+        printf("file succesfully opened with fd : %d\n",fd);
+
+        iRet = write(fd,Buffer,strlen(Buffer));
+
+        printf("%d bytes gets written succesfully\n",iRet);
+
+        close(fd);
+    }
+    
+    return 0;
+}
---
+++ program236.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>       // Required for LINUX OS
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[50] = {'\0'};
+
+    printf("Enter the file name that you want to open : ");
+    scanf("%s",Fname);
+
+    fd = open(Fname, O_RDONLY);        // change
+    if(fd == -1)
+    {
+        printf("Unable to open the file");
+    }
+    else
+    {
+        printf("file succesfully opened with fd : %d\n",fd);
+
+        iRet = read(fd,Buffer,11);
+
+        printf("%d bytes gets read succesfully\n",iRet);
+
+        printf("Data from file is : %s\n",Buffer);
+
+        close(fd);
+    }
+    
+    return 0;
+}
