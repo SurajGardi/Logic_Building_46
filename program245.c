77901cf0943a384e90b04d7294bf100bed7607d9
SurajGardi <surajgardi0707@gmail.com>
2025-06-17 00:06:26 +0530

    problem on File Handling MACRO read() write() memset added

---
+++ program245.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>       // Required for LINUX OS
+#include<unistd.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024        // chk .i file  // once we change macro then all values get changed in all program
+
+int main()
+{
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0, iSum = 0;
+    char Buffer[BUFFER_SIZE] = {'\0'};
+
+    printf("Enter the file name that you want to open : ");
+    scanf("%s",Fname);
+
+    fd = open(Fname, O_RDONLY);        // change
+    if(fd == -1)
+    {
+        printf("Unable to open the file");
+    }
+    else
+    {
+        printf("file succesfully opened with fd : %d\n",fd);
+
+       while((iRet = read(fd,Buffer,BUFFER_SIZE)) != 0)
+       {
+            //printf("%s",Buffer);
+            //write(1,Buffer,iRet);
+            //memset(Buffer,'\0',BUFFER_SIZE);     // for removing Garbage
+
+            iSum = iSum + iRet;
+       }
+
+       printf("File size is : %d bytes\n",iSum);
+
+        close(fd);
+    }
+    
+    return 0;
+}
